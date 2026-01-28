9e843dc0280ce6c1c4b62ebfd2864ec1ebbb0d64
Joshua Turner <joshturner1707@yahoo.co.uk>
2026-01-28 17:48:28 +0000

    fixing style issue with login button

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('placeholder/', lambda r: None, name='placeholder'),  # Placeholder URL pattern
+    # This is linked to a button in the base.html template that has a tag "action = {% url 'login' %}"
+    # When the button is clicked, login_view function in views.py is called which renders login.html
+    path('login/', views.login_view, name='login'),
 ]
