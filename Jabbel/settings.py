86962420f3c144ca37ea91a742cee0a4bb35c6a1
Joshua Turner <joshturner1707@yahoo.co.uk>
2026-01-28 14:36:55 +0000

    Creating users and journals apps

--- Jabbel/settings.py
+++ Jabbel/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'Jabbel.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
@@ -114,6 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+# Connect static folder
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
