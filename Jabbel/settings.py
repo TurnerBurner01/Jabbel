69e2deec766cdf33d80e3c84357f2074562af5d0
Joshua Turner <joshturner1707@yahoo.co.uk>
2026-01-28 17:57:38 +0000

    Back button takes the user back to the home page

--- Jabbel/settings.py
+++ Jabbel/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Jabbel.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'], # connect temapltes folder
+        'DIRS': [BASE_DIR / 'templates'], # connect templates folder
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- journals/urls.py
+++ journals/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+# This is so i can referecne this function in other templates using 'journals:home'
+app_name = 'journals'
+
 urlpatterns = [
     # This calls the home function in views.py in journals app when the root URL is accessed (' ')
     path('', views.home, name='home'), 
