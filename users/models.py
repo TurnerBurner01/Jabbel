1ee54da6591561226bb6c707453a57d2146409b3
Joshua Turner <joshturner1707@yahoo.co.uk>
2026-02-02 18:16:18 +0000

    Updating username field to allow spaces and certain chars

--- users/models.py
+++ users/models.py
@@ -1,8 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
 # Create your models here.
 class User(AbstractUser):
+
+    # Allows spaces and certain special characters in username
+    username_validator = RegexValidator(
+        regex=r'^[\w\s.\'-]+$',
+    )
+
+    # Apply the custom validator to the username field
+    username = models.CharField(
+        max_length=150,
+        unique=False, 
+        validators=[username_validator],
+        help_text='Enter your full name or a display name.'
+    )
     
     # Define email field as unique
     email = models.EmailField(unique=True)
