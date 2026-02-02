4d798d0143e9543f308f0b2c57d23871e6b8fa19
Joshua Turner <joshturner1707@yahoo.co.uk>
2026-02-02 17:16:17 +0000

    Refining comment

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,8 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    # This is linked to a button in the base.html template that has a tag "action = {% url 'register' %}"
-    # When the button is clicked, register_view function in views.py is called which renders register.html
+    # This is linked to a button in the base.html template that has a tag "action = {% url 'users:register' %}"
+    # When the button is clicked, register_view function in views.py is called which renders login.html
     path('register/', views.register_view, name='register'),
+    # path('login/', views.register_view, name='register'),
 ]
