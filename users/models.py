bb8bfee564171331bb8e7447b2a754bf23625925
Joshua Turner <joshturner1707@yahoo.co.uk>
2026-02-02 18:23:01 +0000

    Updating users db to allow custom validation on the username for displaying and cleaning the name and email

--- users/models.py
+++ users/models.py
@@ -30,5 +30,14 @@ class User(AbstractUser):
     # Image field for profile picture
     profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
 
+    # Clean method to normalize email and trim username
+    def clean(self):
+        super().clean()
+        if self.username:
+            self.username = " ".join(self.username.split())
+        if self.email:
+            self.email = self.__class__.objects.normalize_email(self.email)
+
+    # String representation of the user
     def __str__(self):
-        return self.email
+        return f"{self.username} ({self.email})"
