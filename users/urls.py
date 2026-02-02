246a0c072becc0a3ca5b754a2c04daba1ee3ce04
Joshua Turner <joshturner1707@yahoo.co.uk>
2026-02-02 17:49:06 +0000

    Creating and connecting register and login view functions

--- users/urls.py
+++ users/urls.py
@@ -4,8 +4,7 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    # This is linked to a button in the base.html template that has a tag "action = {% url 'users:register' %}"
-    # When the button is clicked, register_view function in views.py is called which renders login.html
+    # This is linked to a button in the base.html template that has a tag "action = {% url 'users:register or login' %}"
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
 ]
