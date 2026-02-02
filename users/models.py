823a83f50a997012bcb90b4e452a26f1d793faa6
Joshua Turner <joshturner1707@yahoo.co.uk>
2026-02-02 14:50:19 +0000

    Creating Users Model class to define the db fields

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class Users(AbstractUser):
+    
+    # Define email field as unique
+    emails = models.EmailField(unique=True)
+
+    # Set email as the login identifier
+    USERNAME_FIELD = "email"
+
+    # This makes 'username' required when running 'createsuperuser'
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return self.email
