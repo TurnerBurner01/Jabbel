70576665e0d4aa40c0b3cf0c73f17a2a8d4257af
Joshua Turner <joshturner1707@yahoo.co.uk>
2026-02-02 15:24:02 +0000

    Editing email fields spelling || Addingprofile picture field

--- users/models.py
+++ users/models.py
@@ -5,13 +5,16 @@ from django.contrib.auth.models import AbstractUser
 class Users(AbstractUser):
     
     # Define email field as unique
-    emails = models.EmailField(unique=True)
+    email = models.EmailField(unique=True)
 
     # Set email as the login identifier
     USERNAME_FIELD = "email"
 
     # This makes 'username' required when running 'createsuperuser'
     REQUIRED_FIELDS = ["username"]
+
+    # Image field for profile picture
+    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
 
     def __str__(self):
         return self.email
