b11d3316ab2c6a4302cacdae5502709b84455e1b
Joshua Turner <joshturner1707@yahoo.co.uk>
2026-02-03 16:26:46 +0000

    Created Journal CRUD functions for future logic

--- journals/views.py
+++ journals/views.py
@@ -1,5 +1,47 @@
+'''
+This is where all the crud (create, read, update, delete) operations for journals will be handled.
+Completed:
+- Read: listJournals() - lists all journals for the logged-in user
+'''
+
 from django.shortcuts import render
 
-# Create your views here.
+# Home page view
 def home(request):
     return render(request, 'journals/home.html')
+
+
+
+# Creates a new journal entry (not yet implemented)
+def createJournal(request):
+    if request.method == 'POST':
+        pass
+
+
+
+# Edits an existing journal entry (not yet implemented)
+def editJournal(request, journal_id):
+    if request.method == 'POST':
+        pass
+
+
+
+# Deletes a journal entry (not yet implemented)
+def deleteJournal(request, journal_id):
+    if request.method == 'POST':
+        pass
+
+
+
+# Gathers and displays a list of journals for the logged-in user
+def listJournals(request):
+
+    if request.method == 'GET':
+        journals = request.user.journals.all()  # Get all journals for the current user
+        context = {
+            'journals': journals
+        }
+        return render(request, 'journals/listJournals.html', context)
+    
+    else:
+        return render(request, 'journals/listJournals.html')
